a111c8a608171531748daeccb59cf64cf9a712e7
Quenwaz <zhangkunhua3696@126.com>
2021-08-16 22:06:11 +0800

    Improve qservice.

--- src/server/impl/qservice_mt.cpp
+++ src/server/impl/qservice_mt.cpp
@@ -1,5 +1,31 @@
 #include "qservice_mt.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
+
+namespace __internal{
+
+struct thread_data{
+    int socket{-1};
+    qservice::socket::QServiceMT* data{nullptr};
+};
+
+void* thread_proc(void* data)
+{
+    thread_data* thdata_ptr = static_cast<thread_data*>(data);
+    if (thdata_ptr == nullptr){
+        return nullptr;
+    }
+
+    for (;;)
+    {
+        ::recv(thdata_ptr->socket, );
+    }
+    
+    return nullptr;
+}
+}
 
 
 qservice::socket::QServiceMT::QServiceMT(const char* host, unsigned int port)
@@ -11,5 +37,23 @@ qservice::socket::QServiceMT::QServiceMT(const char* host, unsigned int port)
 
 void qservice::socket::QServiceMT::run()
 {
+    for (;;)
+    {
+        struct sockaddr_in addr_client;
+        socklen_t n_addr_client = sizeof(struct sockaddr_in);
+        int socket_client = accept(this->socket_, (struct sockaddr*)&addr_client, &n_addr_client);
+        if (socket_client == -1){
+            continue;
+        } 
+
+		pthread_t thread;
+		if(0 > pthread_create(&thread, NULL, __internal::thread_proc, &socket_client))
+		{
+			continue;
+		}
+
+        pthread_detach(thread);
+    }
     
+
 }
